51ee32aceab2e96f56bfd40d175e9ec30aaa80c6
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-04-25 03:46:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 27961050  # api id
 API_HASH = "9e5ca9e6da48a36c102ae9c8c8abac9f"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "6998368630:AAFhK8ISGOMKN5Z_QQ-KbmFE4rCfxqtTFcc"  # bot token
 
 
 ## REDIS
