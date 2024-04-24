9d8701d6a018752a070c546bee424985dccf8ffa
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-04-25 03:45:12 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = 27961050  # api id
+API_HASH = "9e5ca9e6da48a36c102ae9c8c8abac9f"  # api hash
 
 BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
@@ -9,6 +9,6 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+PRIVATE_CHAT_ID = -1002099817426  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "{'_ga': 'GA1.1.1211427720.1712288294', 'stripe_sid': 'd034eabe-581a-4ec0-8626-cff625660327b2ad09', 'g_state': '{"i_l":0}', 'bid_n': '18eac5c78d52769bd34207', 'browserid': 'FzoVyvGEWTZ4PqtIxXLHM5h1u0NPpT24GICAuQG_3JfxVvbqaWqG_fBAmnY=', '__stripe_mid': '41f26875-1bb0-4a4c-9e9d-325915a9152ca35c87', 'lang': 'en', '_ga_RSNVN63CM3': 'GS1.1.1712288293.1.0.1712288675.60.0.0', 'csrfToken': 'u149Lpp2VQHoAAbKNBsRLzwK', '_ga_06ZNKL8C2E': 'GS1.1.1713587860.8.1.1713587866.54.0.0', 'ndus': 'YyyTi3eteHuiGkmRmeY-2-fTOY0qMEuBM1cvIzCN', 'ndut_fmt': '65150E750CF7D95E2F3C5B91E0B2209C55C3B8D48E47032DD6B5834EA0C92D8B','ab_bid':'2e600299fec4c5699a488db87a44bf67de01','ab_jid':'652b2bd000aef41ee0ab25ce44682e600299','ab_sr':'1.0.1_OTY2MWI4YWQ2MzE0MzA4NTJmOTg4MjIwOTQ4OWM4MTMzNGM5YTg0MWU3OTcwZDFiZTc3ZDRkOWJmMmQwNGExNWZhZWQwODk5MmQwY2JlMWEwNWRjYjk3Y2QyMTRhZDUwY2UzYjY3OWRmN2FlYWExZTM3YmI4MzNiMTgxNjVlZWVjMTZlY2I2Y2U3OTQ1YjJjZWVmYTg3NmVhMTQzYzQzZQ=='}"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+ADMINS = [6736190595]
