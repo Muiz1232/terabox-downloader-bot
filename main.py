c439c31a0e4dfd10060b22256784751f5b08f3e5
Muiz1232 <167227414+Muiz1232@users.noreply.github.com>
2024-04-28 04:05:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -45,15 +45,14 @@ async def start(m: UpdateNewMessage):
     reply_text = f"""
 Hello! I am a bot to download videos from terabox.
 Send me the terabox link and I will start downloading it.
-Join @RoldexVerse For Updates
-[Source Code](https://github.com/r0ld3x/terabox-downloader-bot) """
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+Join @Tech_Muiz_Official For Updates"""
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @Tech_Muiz_Official then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @Tech_Muiz_Official then send me the link again."
         )
     await m.reply(reply_text, link_preview=False, parse_mode="markdown")
 
@@ -69,13 +68,13 @@ Join @RoldexVerse For Updates
 async def start(m: UpdateNewMessage):
     text = m.pattern_match.group(1)
     fileid = db.get(str(text))
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @Tech_Muiz_Official then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @Tech_Muiz_Official then send me the link again."
         )
     await bot(
         ForwardMessagesRequest(
@@ -83,7 +82,7 @@ async def start(m: UpdateNewMessage):
             id=[int(fileid)],
             to_peer=m.chat.id,
             drop_author=True,
-            # noforwards=True,  # Uncomment it if you dont want to forward the media.
+            noforwards=True,  # Uncomment it if you dont want to forward the media.
             background=True,
             drop_media_captions=False,
             with_my_score=True,
@@ -126,16 +125,16 @@ async def handle_message(m: Message):
     url = get_urls_from_string(m.text)
     if not url:
         return await m.reply("Please enter a valid url.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerse", m.peer_id)
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
-        return await m.reply("Please join @RoldexVerse then send me the link again.")
-    check_if = await is_user_on_chat(bot, "@RoldexVerseChats", m.peer_id)
+        return await m.reply("Please join @Tech_Muiz_Official then send me the link again.")
+    check_if = await is_user_on_chat(bot, "@Tech_Muiz_Official", m.peer_id)
     if not check_if:
         return await m.reply(
-            "Please join @RoldexVerseChats then send me the link again."
+            "Please join @Tech_Muiz_Official then send me the link again."
         )
     is_spam = db.get(m.sender_id)
-    if is_spam and m.sender_id not in [1317173146]:
+    if is_spam and m.sender_id not in [5843408012]:
         return await m.reply("You are spamming. Please wait a 1 minute and try again.")
     hm = await m.reply("Sending you the media wait...")
     count = db.get(f"check_{m.sender_id}")
@@ -159,7 +158,7 @@ async def handle_message(m: Message):
                 id=[int(fileid)],
                 to_peer=m.chat.id,
                 drop_author=True,
-                # noforwards=True, #Uncomment it if you dont want to forward the media.
+                noforwards=True, #Uncomment it if you dont want to forward the media.
                 background=True,
                 drop_media_captions=False,
                 with_my_score=True,
@@ -187,7 +186,7 @@ async def handle_message(m: Message):
         return await hm.edit(
             f"Sorry! File is not supported for now. I can download only .mp4, .mkv and .webm files."
         )
-    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [1317173146]:
+    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [5843408012]:
         return await hm.edit(
             f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} ."
         )
@@ -237,9 +236,9 @@ async def handle_message(m: Message):
             caption=f"""
 File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
-Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
+Direct Link: [Click Here](https://t.me/teraboxtech_bot?start={uuid})
 
-@RoldexVerse
+@Tech_Muiz_Official
 """,
             supports_streaming=True,
             spoiler=True,
@@ -261,9 +260,9 @@ Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
             caption=f"""
 File Name: `{data['file_name']}`
 Size: **{data["size"]}** 
-Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
+Direct Link: [Click Here](https://t.me/teraboxtech_bot?start={uuid})
 
-@RoldexVerse
+@Tech_Muiz_Official
 """,
             progress_callback=progress_bar,
             thumb=thumbnail if thumbnail else None,
@@ -300,7 +299,7 @@ Direct Link: [Click Here](https://t.me/teraboxdown_bot?start={uuid})
                 to_peer=m.chat.id,
                 top_msg_id=m.id,
                 drop_author=True,
-                # noforwards=True,  #Uncomment it if you dont want to forward the media.
+                noforwards=True,  #Uncomment it if you dont want to forward the media.
                 background=True,
                 drop_media_captions=False,
                 with_my_score=True,
